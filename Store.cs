cfabee73e2c7edbf4514eea8407bbd5bfaa19293
Thane <thabay0606@students.maquoketaschools.org>
2026-03-02 14:17:32 -0600

    Should be goood.

--- Store.cs
+++ Store.cs
@@ -2,4 +2,17 @@
 public class Store
 {
     // Add your code here
+    public string PriceCheck(double price) {
+        if (price == 1.50){
+            return "match";
+        }
+        return "";
+    }
+
+    public string GuessNumber(int guess) {
+        if (guess == 25){
+            return "";
+        }
+        return "not it";
+    }
 }
